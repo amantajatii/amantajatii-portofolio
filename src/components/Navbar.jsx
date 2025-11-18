bde49c58b239cd487a48ab121f985cf8777dbdbe
Diaz Amantajati Susilo <diazamantajatisusilo@mail.ugm.ac.id>
2025-11-18 11:43:49 +0700

    Update Navbar links and contact button
    Changed 'Journey' and 'Projects' links to point to their respective routes. Wrapped the 'Contact me' button in a Link that opens Gmail with a pre-filled recipient.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,15 +51,17 @@ const Navbar = () => {
                 <Link href="/">Home</Link>
               </li>
               <li>
-                <Link href="/">Journey</Link>
+                <Link href="/journey">Journey</Link>
               </li>
               <li>
-                <Link href="/">Projects</Link>
+                <Link href="/projects">Projects</Link>
               </li>
               <li>
-                <button className="px-4 py-2 bg-[#18857e] rounded-lg text-white shadow-md hover:bg-[#159c95] transition cursor-pointer">
-                  Contact me
-                </button>
+                <Link href="https://mail.google.com/mail/u/0/?fs=1&to=amantajati15@gmail.com&tf=cm">
+                  <button className="px-4 py-2 bg-[#18857e] rounded-lg text-white shadow-md hover:bg-[#159c95] transition">
+                    Contact me
+                  </button>
+                </Link>
               </li>
             </ul>
           </div>
